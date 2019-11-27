ae3e2c93a57a2d1c107569f0397fd771c46bd3f2
muhammadhussain-code <muhammadhussain99100@gmail.com>
2019-11-27 11:02:31 +0500

    JS todo app

---
+++ index.js
@@ -0,0 +1,90 @@
+// function collecting(e){
+//     e.preventDefault();
+//     var asd =new FormData(e.target)
+//     var daata = {
+//         title: asd.get('title'),
+//         description: asd.get('des')
+//     }
+
+//     document.getElementById('todo').innerHTML += `
+//     <div class="row">
+//                 <div class="col-md-4 container">
+//                     <p class="h2">${daata.title}.</p>
+//                     <div class="row ml-2">
+//                         <p class="h6 pt-4">${daata.description} </p>
+
+//                         <input type="button" value="delete" class="btn btn-danger py-auto" onclick="dlte(event)" style="padding: -10px 5px -10px 5px!important">
+//                     </div>
+//                 </div>
+//             </div>
+    
+//     `
+//    e.target.reset();
+// }
+
+// function dlte(e){
+//     e.target.parentNode.parentNode.parentNode.remove()
+// }
+var arr = JSON.parse(localStorage.getItem('todos'));
+if(arr !== null) {
+   arr.map(value => {
+    document.getElementById('todo').innerHTML += `
+    <div class="row">
+                <div class="col-md-4 container">
+                    <p class="h2">${value.title}</p>
+                    <div class="row ml-2">
+                        <p class="h6 pt-4">${value.description} </p>
+
+                        <input type="button" value="delete" class="btn btn-danger py-auto" onclick="dlte(event)" style="padding: -10px 5px -10px 5px!important">
+                    </div>
+                </div>
+            </div>
+    
+    `
+   })
+   
+   
+}
+else {
+    arr = [];
+}
+
+function collecting(e){
+        e.preventDefault();
+        var asd =new FormData(e.target)
+        var daata = {
+            title: asd.get('title'),
+            description: asd.get('des')
+        }
+        arr.push(daata);
+        document.getElementById('todo').innerHTML += `
+        <div class="row">
+                    <div class="col-md-4 container">
+                        <p class="h2">${daata.title}</p>
+                        <div class="row ml-2">
+                            <p class="h6 pt-4">${daata.description} </p>
+    
+                            <input type="button" value="delete" class="btn btn-danger py-auto" onclick="dlte(event)" style="padding: -10px 5px -10px 5px!important">
+                        </div>
+                    </div>
+                </div>
+        
+        `
+        localStorage.setItem('todos', JSON.stringify(arr));
+       e.target.reset();
+    }
+    
+    function dlte(e){
+        // console.log(e.target.parentNode.previousElementSibling.innerText)
+        arr.map((value, index) => {
+            if(value.title === e.target.parentNode.previousElementSibling.innerText) {
+                arr.splice(index, 1);
+            }
+        });
+        localStorage.setItem('todos', JSON.stringify(arr));
+        e.target.parentNode.parentNode.parentNode.remove();
+        
+    }
+
+    // localStorage.removeItem('todos')
+
