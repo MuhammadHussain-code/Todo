936f5f6deeb48a07a685fe4311d16c00daf24cdd
muhammadhussain-code <muhammadhussain99100@gmail.com>
2019-11-30 17:17:27 +0500

    just added emailJS

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 //                     </div>
 //                 </div>
 //             </div>
-    
+
 //     `
 //    e.target.reset();
 // }
@@ -25,66 +25,78 @@
 // function dlte(e){
 //     e.target.parentNode.parentNode.parentNode.remove()
 // }
+
+
+
 var arr = JSON.parse(localStorage.getItem('todos'));
-if(arr !== null) {
-   arr.map(value => {
-    document.getElementById('todo').innerHTML += `
+
+if (arr !== null) {
+    arr.map(value => {
+        document.getElementById('todo').innerHTML += `
     <div class="row">
                 <div class="col-md-4 container">
                     <p class="h2">${value.title}</p>
                     <div class="row ml-2">
                         <p class="h6 pt-4">${value.description} </p>
-
                         <input type="button" value="delete" class="btn btn-danger py-auto" onclick="dlte(event)" style="padding: -10px 5px -10px 5px!important">
                     </div>
                 </div>
             </div>
     
     `
-   })
-   
-   
+    })
+
+
 }
 else {
     arr = [];
 }
 
-function collecting(e){
-        e.preventDefault();
-        var asd =new FormData(e.target)
-        var daata = {
-            title: asd.get('title'),
-            description: asd.get('des')
-        }
-        arr.push(daata);
-        document.getElementById('todo').innerHTML += `
+function collecting(e) {
+    e.preventDefault();
+    var asd = new FormData(e.target)
+    var daata = {
+        title: asd.get('title'),
+        description: asd.get('des'),
+        time:asd.get('time')
+    }
+    arr.push(daata);
+    document.getElementById('todo').innerHTML += `
         <div class="row">
                     <div class="col-md-4 container">
                         <p class="h2">${daata.title}</p>
                         <div class="row ml-2">
                             <p class="h6 pt-4">${daata.description} </p>
-    
-                            <input type="button" value="delete" class="btn btn-danger py-auto" onclick="dlte(event)" style="padding: -10px 5px -10px 5px!important">
+                        
+                            <input type="button" value="XXX" class="btn btn-danger " onclick="dlte(event)" style="padding: -100px 5px -100px 5px!important">
                         </div>
                     </div>
                 </div>
         
         `
-        localStorage.setItem('todos', JSON.stringify(arr));
-       e.target.reset();
-    }
+    localStorage.setItem('todos', JSON.stringify(arr));
+    e.target.reset();
     
-    function dlte(e){
-        // console.log(e.target.parentNode.previousElementSibling.innerText)
-        arr.map((value, index) => {
-            if(value.title === e.target.parentNode.previousElementSibling.innerText) {
-                arr.splice(index, 1);
-            }
-        });
-        localStorage.setItem('todos', JSON.stringify(arr));
-        e.target.parentNode.parentNode.parentNode.remove();
-        
-    }
+    // emailjs.send("gmail","template_E3pvnun8",daata,"user_1sDcp81XGM3DitGEqDppP")
+    //   .then((resp) => {
+    //     // successCallback();
+    //     console.log('FIRE EMAIL SUCCESS!', resp.status, resp.text);
+    //   }, (err) => {
+    //     // failureCallback();
+    //      console.log('FIRE EMAIL FAILED...', err);
+    //   });
+}
+
+function dlte(e) {
+    // console.log(e.target.parentNode.previousElementSibling.innerText)
+    arr.map((value, index) => {
+        if (value.title === e.target.parentNode.previousElementSibling.innerText) {
+            arr.splice(index, 1);
+        }
+    });
+    localStorage.setItem('todos', JSON.stringify(arr));
+    e.target.parentNode.parentNode.parentNode.remove();
+
+}
 
     // localStorage.removeItem('todos')
-
